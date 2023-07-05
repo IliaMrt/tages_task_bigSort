963b34270622b0e8615eeb6ef0c2c0642d92f728
IliaMrt <ilya,martens@protonmail.com>
2023-07-05 09:44:50 +0300

    применил линтер

--- check.js
+++ check.js
@@ -1,60 +1,56 @@
-import {open} from "fs/promises";
+import { open } from 'fs/promises'
 
 let linesCount = 0
 let errorsCount = 0
 
-function readLines(input, func) {
+function readLines (input, func) {
+  let remaining = ''
 
-    let remaining = '';
+  input.on('data', function (data) {
+    remaining += data
+    let index = remaining.indexOf('\n')
 
-    input.on('data', function (data) {
+    while (index > -1) {
+      const line = remaining.substring(0, index)
+      remaining = remaining.substring(index + 1)
+      func(line)
+      index = remaining.indexOf('\n')
+    }
+  })
 
-        remaining += data;
-        let index = remaining.indexOf('\n');
-
-        while (index > -1) {
-            let line = remaining.substring(0, index);
-            remaining = remaining.substring(index + 1);
-            func(line);
-            index = remaining.indexOf('\n');
-        }
-    });
-
-    input.on('end', function () {
-        if (remaining.length > 0) {
-            func(remaining);
-        }
-    });
+  input.on('end', function () {
+    if (remaining.length > 0) {
+      func(remaining)
+    }
+  })
 }
 
-async function check(fileName) {
+async function check (fileName) {
+  const file = await open(fileName, 'r')
+  const stream = file.createReadStream({ encoding: 'utf-8' })
+  let previous = ''
 
-    const file = await open(fileName, 'r');
-    const stream = file.createReadStream({encoding: 'utf-8'});
-    let previous = ''
+  await readLines(stream, (current) => {
+    linesCount++
+    let i = 0
+    let stop = false
 
-    await readLines(stream, (current) => {
+    let compareResult = previous.length <= current.length
 
-        linesCount++
-        let i = 0
-        let stop = false
+    do {
+      if (current[i] !== previous[i]) {
+        compareResult = previous[i]?.charCodeAt(0) < current[i]?.charCodeAt(0)
+        stop = true
+      }
+      i++
+    } while ((i < Math.min(current.length, previous.length, 10)) && !stop)
 
-        let compareResult = previous.length <= current.length
+    if (!compareResult) {
+      console.log(`Err #${++errorsCount}, line ${linesCount}: prev=${previous.slice(0, 10)}, curr=${current.slice(0, 10)}`)
+    }
 
-        do {
-            if (current[i] != previous[i]) {
-                compareResult = previous[i]?.charCodeAt(0) < current[i]?.charCodeAt(0)
-                stop = true
-            }
-            i++
-        } while ((i < Math.min(current.length, previous.length, 10)) && !stop)
-
-        if (!compareResult) {
-            console.log(`Err #${++errorsCount}, line ${linesCount}: prev=${previous.slice(0, 10)}, curr=${current.slice(0, 10)}`)
-        }
-
-        previous = current
-    })
+    previous = current
+  })
 }
 
 await check(process.argv[2])
